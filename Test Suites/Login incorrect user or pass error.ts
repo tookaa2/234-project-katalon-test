5801c4e5bb28111890fb92ea11fda7e5fafa1bf6
Donut <donutzue@gmail.com>
2018-04-28 23:48:25 +0700

    Created test case for log in with wrong infos, validate if the error message is present

---
+++ Test Suites/Login incorrect user or pass error.ts
@@ -0,0 +1,50 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description>Incorrect login test</description>
+   <name>Login incorrect user or pass error</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <lastRun>2018-04-28T23:46:11</lastRun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>71819bb6-2d60-472c-8c6e-2d0b4aa1d181</testSuiteGuid>
+   <testCaseLink>
+      <guid>11501c72-c192-4064-ac86-857b305cde0b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Navigate to web</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>b4e3a5bf-8889-4161-9881-44d9eb65390a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login with credentials</testCaseId>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>SCRIPT_VARIABLE</type>
+         <value>&quot;abc&quot;</value>
+         <variableId>d23196c2-519d-4e28-96bb-05a21bdb9cf9</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>SCRIPT_VARIABLE</type>
+         <value>&quot;abc&quot;</value>
+         <variableId>0bc0a367-2c7b-477e-8476-6d9e80ef7936</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>da21ab84-e0c7-4061-8e3a-a0567b1b911e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login error verify</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>2984e7cb-31b4-4991-ae4e-326afd3c1848</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Exit browser</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
